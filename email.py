d046e1c84ca7cbf1a25a604b0101d61b62ec2c93
Beren Millidge <bmillidgework@gmail.com>
2018-10-22 23:44:11 +0100

    Add files via upload

--- email.py
+++ email.py
@@ -15,6 +15,8 @@ f.close()
 #get variables
 from_email, password, to_email = credential_string.split(credential_spliter)
 
+PORT = 465 # arbitrarily from stack overflow
+
 
 # I'll also put the logging capabilities here! for now!
 
@@ -44,18 +46,18 @@ def log(message):
 
 
 
-def send_mail(subject, message):
+def send_mail(subject, message, port=PORT):
 	# just recreate the server every time... because! it's probaly really bad... but doesn't matter!
 	try:
 		msg = MIMEMultipart()
 		msg['Subject'] = subject
-		msg['From'] = me
-		msg['To'] = you
+		msg['From'] = from_email
+		msg['To'] = to_email
 		msg.attach(MIMEText(message, 'plain'))
-		server = smtplib.SMTP_SSL('smtp.gmail.com', 465) # presumably arbitrary port number
+		server = smtplib.SMTP_SSL('smtp.gmail.com', port) # presumably arbitrary port number
 		server.ehlo()
 		server.login(from_email, password)
-		server.sendmail(me, [to_email], msg.as_string())
+		server.sendmail(from_email, [to_email], msg.as_string())
 		server.close()
 		print("Message Sent!")
 		log(format_message_string("Email Sent"))
